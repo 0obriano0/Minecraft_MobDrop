9cd25c198c33aba6cda38ce2f74507bdf10a5b05
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2019-04-20 13:57:27 +0800

    updata

--- src/com/brian/MobDrop/Database/DataBase.java
+++ src/com/brian/MobDrop/Database/DataBase.java
@@ -15,54 +15,54 @@ import com.brian.MobDrop.LoadFile.LoadLanguage;
 
 public class DataBase {
 	// 主插件
-		public static Main main;
-		
-		// 設定檔
-		public static LoadConfig LoadConfig;
-		public static LoadItems LoadItems;
-		public static LoadMobs LoadMobs;
-		public static LoadLanguage LoadLanguage;
-		
-		// 伺服器
-		public static Server server;
-		
-		//語言包
-		public static language language = new language();
-		
-		// 插件目錄
-		public static String pluginMainDir = "./plugins/MobDrop/";
-		
-		//指令目錄
-		public static MainList CommandsList = new MainList();
+	public static Main main;
+	
+	// 設定檔
+	public static LoadConfig LoadConfig;
+	public static LoadItems LoadItems;
+	public static LoadMobs LoadMobs;
+	public static LoadLanguage LoadLanguage;
+	
+	// 伺服器
+	public static Server server;
+	
+	//語言包
+	public static language language = new language();
+	
+	// 插件目錄
+	public static String pluginMainDir = "./plugins/MobDrop/";
+	
+	//指令目錄
+	public static MainList CommandsList = new MainList();
 
-		//掉落物品清單
-		public static Map<String, List<MobItemList>> MobItemMap = new HashMap<String, List<MobItemList>>();
-		
-		//物品設定
-		public static Map<String, Items> ItemMap = new HashMap<String, Items>();
-		
-		//公開顯示訊息
-		public static Config Config;
-		
-		// 顯示訊息
-		public static void Print(String msg)
-		{
-			System.out.print("[MobDrop] " + msg);
+	//掉落物品清單
+	public static Map<String, List<MobItemList>> MobItemMap = new HashMap<String, List<MobItemList>>();
+	
+	//物品設定
+	public static Map<String, Items> ItemMap = new HashMap<String, Items>();
+	
+	//公開顯示訊息
+	public static Config Config;
+	
+	// 顯示訊息
+	public static void Print(String msg)
+	{
+		System.out.print("[MobDrop] " + msg);
+	}
+	
+	// 取得生物名稱(中文)
+	public static String GetEntityName(String entityId){
+		if(DataBase.language.IDMobtoMessage.containsKey(entityId))
+			return DataBase.language.IDMobtoMessage.get(entityId);
+		else
+			return entityId;
+	}
+	
+	// 取得生物名稱(中文)
+	public static String getEntityNameGameCode(String entityId){
+		if(DataBase.language.MessagetoIDMob.containsKey(entityId))
+			return DataBase.language.MessagetoIDMob.get(entityId);
+		else
+			return entityId;
 		}
-		
-		// 取得生物名稱(中文)
-		public static String GetEntityName(String entityId){
-			if(DataBase.language.IDMobtoMessage.containsKey(entityId))
-				return DataBase.language.IDMobtoMessage.get(entityId);
-			else
-				return entityId;
-		}
-		
-		// 取得生物名稱(中文)
-		public static String getEntityNameGameCode(String entityId){
-			if(DataBase.language.MessagetoIDMob.containsKey(entityId))
-				return DataBase.language.MessagetoIDMob.get(entityId);
-			else
-				return entityId;
-			}
 }
